a3e0081c01f7e0b2c022e094c30236dec7cdbf2b
Sergio Barrera <sdbeng@gmail.com>
2014-10-30 12:25:56 -0700

    Initial check-in

---
+++ app.js
@@ -0,0 +1,24 @@
+/*
+Combine file_read.js with app.js
+*/
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(request, response) {
+  response.writeHead(200);
+  fs.readFile('index.html', function(err, contents){
+  	response.end(contents);
+    response.end();
+  });
+  
+}).listen(8080);
+console.log("node server running port 8080...");
+
+//Basic server example
+// var http = require('http');
+
+// http.createServer(function(request, response) {
+//   response.writeHead(200);
+//   response.end("Hello, this is Dani boy");
+// }).listen(8080);
+// console.log("node server running port 8080...");
