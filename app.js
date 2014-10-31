cce0c4eb765599fe66d12a0b7385339e976f70f3
Sergio Barrera <sdbeng@gmail.com>
2014-10-31 09:08:44 -0700

    Update gitignore

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-  response.writeHead(200);
+  response.writeHead(200, {'Content-Type': 'text/html'});
   fs.readFile('index.html', function(err, contents){
   	response.end(contents);
     response.end();
